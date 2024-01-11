b8d06a454d7ffb6070ce4f16fc314e1ef74b5e4e
Rawan Aladdin <raladdin@my.bcit.ca>
2024-01-10 16:31:43 -0800

    form done on index

--- Form/index.js
+++ Form/index.js
@@ -1,8 +1,24 @@
 export default function Form() {
     return(
         <>
-            <div>
-            
+            <div className={styles.formOuterContainer}>
+                <div className={styles.formContainer}>
+                    <form className={styles.form}>
+                        <fieldset className={styles.fieldSet}>
+                            <input 
+                                id="username"
+                                className={styles.userNameInput}
+                                type="text"
+                            />
+                            <input 
+                                id="password"
+                                className={styles.passwordInput}
+                                type="password"
+                            />
+                        </fieldset>
+                    </form>
+                    
+                </div>
             </div>
         </>
     )
